7808c99a3dd9517d35ed35f9791e2a8acaa70222
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-10-03 21:01:52 -0500

    alter config connections

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/kitchen_hero',
+    DB_URL: process.env.DB_URL || 'https://fierce-hollows-91839.herokuapp.com/',
     API_BASE_URL: process.env.REACT_APP_BASE_URL || 
-      "http://localhost:3000/api"
+      "https://kitchen-hero.kayleighkat98.vercel.app"
   }
