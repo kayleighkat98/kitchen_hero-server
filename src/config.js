2fbc9e463d667e98a7567b2f3df291853247025f
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-10-05 14:30:11 -0500

    alter react_app_base_url

--- src/config.js
+++ src/config.js
@@ -2,6 +2,5 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL ||'postgresql://postgres@localhost/kitchen_hero',
-    API_BASE_URL: process.env.REACT_APP_BASE_URL || 
-      "https://kitchen-hero.kayleighkat98.vercel.app"
+    API_BASE_URL: process.env.API_BASE_URL || "http://localhost:3000/api",
   }
