b76caaba3fdc7b0228367df9f357b42fcb39822d
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-10-05 14:38:20 -0500

    alter api_base_url

--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL ||'postgresql://postgres@localhost/kitchen_hero',
-    API_BASE_URL: process.env.API_BASE_URL || "http://localhost:3000/api",
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
+    "http://localhost:3000/api"
   }
