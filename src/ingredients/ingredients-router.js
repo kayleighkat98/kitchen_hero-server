3c9b98559de1c2757935e08e554fde92ae3d8797
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-10-06 17:44:02 -0500

    add expired_date to post endpoint

--- src/ingredients/ingredients-router.js
+++ src/ingredients/ingredients-router.js
@@ -30,8 +30,8 @@ IngredientsRouter
   })
   .post(jsonParser, async (req, res, next) => {
     const db = req.app.get('db');
-    const { name, quantity, quantity_type } = req.body;
-    let newIngredient = {name, quantity, quantity_type};
+    const { name, quantity, quantity_type, expiration_date} = req.body;
+    let newIngredient = {name, quantity, quantity_type, expiration_date};
 
     for (const [key, value] of Object.entries(newIngredient)) {
       if (value === null) {
