2291574bf1ee9532b295eac516974f4047ebafa1
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-10-06 15:33:58 -0500

    alter seed table

--- seeds/seed.ingredients.sql
+++ seeds/seed.ingredients.sql
@@ -2,7 +2,7 @@
 
 INSERT INTO ingredients (name, expiration_date, quantity, quantity_type)
 VALUES  
-    ('Apples','2000-10-04', 20, 'pounds' ),
+    ('Apples', null , 20, 'pounds' ),
     ('Soda','2000-11-05', 2, 'liters' ),
     ('Grapes','2020-10-07', 10, 'ounces' ),
     ('Baking Powder','2020-10-30', 5, 'pounds' ),
