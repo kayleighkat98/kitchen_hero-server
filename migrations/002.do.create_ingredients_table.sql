d24b957ff3f952db93f59526eb424529ae1c7ef9
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-10-21 14:22:56 -0500

    add user id to ingredients table *naming syntax clash

--- migrations/002.do.create_ingredients_table.sql
+++ migrations/002.do.create_ingredients_table.sql
@@ -1,5 +1,6 @@
 CREATE TABLE ingredients(
     ingredient_id SERIAL PRIMARY KEY NOT NULL,
+    "user_id" INTEGER REFERENCES "user"(id)ON DELETE CASCADE NOT NULL,
     name TEXT NOT NULL,
     add_date DATE NOT NULL DEFAULT CURRENT_DATE,
     expiration_date DATE,
