723a01212840984bedabf0bb51a035fdac8a00ba
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-11-04 14:56:23 -0600

    remove add date and whitespace

--- migrations/002.do.create_ingredients_table.sql
+++ migrations/002.do.create_ingredients_table.sql
@@ -2,9 +2,7 @@ CREATE TABLE ingredients(
     ingredient_id SERIAL PRIMARY KEY NOT NULL,
     "user_id" INTEGER REFERENCES "user"(id)ON DELETE CASCADE NOT NULL,
     name TEXT NOT NULL,
-    add_date DATE NOT NULL DEFAULT CURRENT_DATE,
-    expiration_date DATE,
+    expiration_date TEXT,
     quantity DECIMAL NOT NULL
-
 );
 
